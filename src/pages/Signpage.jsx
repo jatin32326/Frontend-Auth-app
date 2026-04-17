527a0a797fc2f2e6504eab464f72cbbf34d80023
Jatin Kumar <jatin.32326@gmail.com>
2026-04-17 17:02:33 +0530

    Update signup URL in Signpage component

--- src/pages/Signpage.jsx
+++ src/pages/Signpage.jsx
@@ -27,7 +27,7 @@ function SignUp(){
             return handleError("name, email & password are required")
         }
         try{
-            const url="https://backend-auth-app-one.vercel.app/auth/signup"
+            const url="https://backend-auth-5ncs342cl-jatin32326s-projects.vercel.app/auth/signup"
             const response=await fetch(url,{
                 method:"POST",
                 headers:{
