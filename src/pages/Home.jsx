d19ef989061f666d55c5c02493812823187cebf7
Jatin Kumar <jatin.32326@gmail.com>
2026-04-17 16:37:55 +0530

    Update product fetch URL to production endpoint

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,7 +25,7 @@ function HomePage() {
     
     const fetchProducts=async()=>{
         try{
-            const URL="http://localhost:3000/products";
+            const URL="https://backend-auth-app-one.vercel.app/products";
             const response=  await fetch(URL,headers);
             const headers={
                 headers:{
