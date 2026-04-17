4b2c0251ff4cc35c4ddf3e1bfa475a24b69c001d
Jatin Kumar <jatin.32326@gmail.com>
2026-04-17 16:36:09 +0530

    Update signup URL to production endpoint

--- src/pages/Signpage.jsx
+++ src/pages/Signpage.jsx
@@ -27,7 +27,7 @@ function SignUp(){
             return handleError("name, email & password are required")
         }
         try{
-            const url="http://localhost:3000/auth/signup"
+            const url="https://backend-auth-app-one.vercel.app/auth/signup"
             const response=await fetch(url,{
                 method:"POST",
                 headers:{
