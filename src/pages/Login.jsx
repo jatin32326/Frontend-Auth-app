850268b8717f79503d3823af67fc748f7dc12b73
Jatin Kumar <jatin.32326@gmail.com>
2026-04-17 16:37:02 +0530

    Update login URL to use production endpoint

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -26,7 +26,7 @@ function Login(){
             return handleError(" email & password are required")
         }
         try{
-            const url="http://localhost:3000/auth/login"
+            const url="https://backend-auth-app-one.vercel.app/auth/login"
             const response=await fetch(url,{
                 method:"POST",
                 headers:{
